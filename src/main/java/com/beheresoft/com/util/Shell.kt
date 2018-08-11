f30347fbf5363f16db14833099331ef48d4627b6
Aladi <234369425@qq.com>
2018-08-11 15:09:59 +0800

    exec tools

--- src/main/java/com/beheresoft/com/util/Shell.kt
+++ src/main/java/com/beheresoft/com/util/Shell.kt
@@ -1,11 +1,29 @@
 package com.beheresoft.com.util
 
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
 class Shell private constructor() {
 
     companion object {
-        fun exec(var cmd: String): String {
-            Runtime.getRuntime().exec()
+
+        fun execSilent(cmd: String) {
+            var process = Runtime.getRuntime().exec(cmd)
+            process.waitFor()
         }
+
+        fun exec(cmd: String): String {
+            var result = StringBuilder()
+            val process = Runtime.getRuntime().exec(cmd)
+            var input = BufferedReader(InputStreamReader(process.inputStream))
+            for (line in input.lines()) {
+                result.append(line)
+            }
+            input.close()
+            return result.toString()
+        }
+
+
     }
 
 }
